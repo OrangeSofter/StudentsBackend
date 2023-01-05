a0403c1e0348f5196c5e9cd3cc152db5cec21061
Alex <orangesofter@users.noreply.github.com>
2023-01-05 22:38:33 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "studback.orange.ru.studentsbackend"
---
+++ src/main/kotlin/studback/orange/ru/Application.kt
@@ -0,0 +1,15 @@
+package studback.orange.ru
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import studback.orange.ru.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
